15a16271b31841130cbc295942adf825c411d774
Subrat090 <subratmahapatro09@gmail.com>
2025-05-01 20:45:25 +0530

    userModel

---
+++ backend/app.js
@@ -0,0 +1,15 @@
+
+import express from 'express';
+import morgan from 'morgan';
+import connect from './db/db.js';
+
+connect();
+const app =express();
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+app.use(morgan('dev'));
+app.get('/',(req,res)=>{
+    res.send('hello world');
+});
+
+export default app;
