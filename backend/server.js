2e55496730fed6bd1669d28d788db988a0985528
Subrat090 <subratmahapatro09@gmail.com>
2025-07-12 00:19:16 +0530

    socket implemented

--- backend/server.js
+++ backend/server.js
@@ -1,20 +1,43 @@
 import 'dotenv/config.js';
 import {Server}from 'socket.io';
-
-
 import http from 'http';
 import app from './app.js';
+import jwt from 'jsonwebtoken';
 
 const server = http.createServer(app);
+const port =process.env.PORT || 3000;
+
 
 const io = new Server(server);
+ io.use((socket,next)=>{
+   try{
+const token =
+  socket.handshake.auth?.token ||
+  socket.handshake.headers?.authorization?.split(' ')[1];
+if(!token){
+   return next(new Error('Authentication error'))
+}
+
+const decoded = jwt.verify(token,process.env.JWT_SECRET)
+if(!decoded){
+   return next(new Error('Authentication error'))
+}
+socket.user = decoded
+next();
+   }
+   catch(error){
+      next(error);
+   }
+ })
 io.on('connection', socket => {
-  socket.on('event', data =>{} /* … */); 
- console.log(' a user connected ')
+    console.log(' a user connected ');
+
+  socket.on('event', data =>{/* … */} ); 
+
   socket.on('disconnect', () => { /* … */ }); 
 });
 
-const port =process.env.PORT || 3000;
+
 server.listen(port,()=>{
    console.log(`Server is running on port ${port}`) 
 })
