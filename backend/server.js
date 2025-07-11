d167b77e7187909c04046fc54b576d183d3b44a6
Subrat090 <subratmahapatro09@gmail.com>
2025-07-11 22:35:21 +0530

    socket implemented

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,18 @@
 import 'dotenv/config.js';
+import {Server}from 'socket.io';
 
 
 import http from 'http';
 import app from './app.js';
 
 const server = http.createServer(app);
+
+const io = new Server(server);
+io.on('connection', socket => {
+  socket.on('event', data =>{} /* … */); 
+ console.log(' a user connected ')
+  socket.on('disconnect', () => { /* … */ }); 
+});
 
 const port =process.env.PORT || 3000;
 server.listen(port,()=>{
