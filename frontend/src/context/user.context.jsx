38566fe6c854381a8adf9c94fd1ce26e06481e1b
Subrat090 <subratmahapatro09@gmail.com>
2025-07-30 00:22:36 +0530

    fixed userContext

--- frontend/src/context/user.context.jsx
+++ frontend/src/context/user.context.jsx
@@ -1,13 +1,24 @@
-import React, { createContext, useState, useContext } from 'react';
+import React, { createContext, useState, useEffect } from 'react';
+import axios from '../config/axios.js' 
 
 export const UserContext = createContext();
 
-export const UserProvider =({children})=>{
-    const [user,setUser]=useState(null);
+export const UserProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
 
-    return (
-    <UserContext.Provider value={{user,setUser}}>
-        {children}
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    const storedUser = localStorage.getItem("user");
+
+    if (token && storedUser) {
+      axios.defaults.headers.common['Authorization'] = `Bearer ${token}`;
+      setUser(JSON.parse(storedUser));
+    }
+  }, []);
+
+  return (
+    <UserContext.Provider value={{ user, setUser }}>
+      {children}
     </UserContext.Provider>
-    );
+  );
 };
