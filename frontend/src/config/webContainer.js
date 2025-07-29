ff53386da2a7186970b7a77c77c692451904d8e7
Subrat090 <subratmahapatro09@gmail.com>
2025-07-30 00:06:48 +0530

    updated axios

--- frontend/src/config/webContainer.js
+++ frontend/src/config/webContainer.js
@@ -2,10 +2,14 @@ import { WebContainer } from '@webcontainer/api';
 
 let webContainerInstance = null;
 
-
 export const getWebContainer = async () => {
-    if (webContainerInstance === null) {
-        webContainerInstance = await WebContainer.boot();
+  if (webContainerInstance === null) {
+    try {
+      webContainerInstance = await WebContainer.boot();
+      console.log("WebContainer booted:", webContainerInstance);
+    } catch (error) {
+      console.error("WebContainer boot error:", error);
     }
-    return webContainerInstance;
-}
+  }
+  return webContainerInstance;
+};
